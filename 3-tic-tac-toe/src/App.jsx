380675fa424bf7a5974003cda0e5036e578115f5
koctaa04 <yeftaocta04@gmail.com>
2024-01-22 01:07:57 +0700

    add file tic tac toe

---
+++ 3-tic-tac-toe/src/App.jsx
@@ -0,0 +1,86 @@
+import { useState } from 'react'
+import './index.css'
+
+
+// eslint-disable-next-line react/prop-types
+function Square({value, onSquareClick}) {
+  // const [value, setValue] = useState('');
+
+  
+  return(
+    <>
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+    </>
+  )
+}
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  function handleClick(i) {    
+    if (squares[i] || calculateWinner(squares)) return;
+
+    const newSquares = squares.slice();
+    newSquares[i] = xIsNext ? 'X' : 'O';
+    setSquares(newSquares);
+    setXIsNext(!xIsNext);
+
+    // if (newSquares[i] ) {
+    //   newSquares[i] = null;
+    // } else {
+    //   newSquares[i] = 'X';
+    // }
+    //   setSquares(newSquares);
+  }
+
+  const winner = calculateWinner(squares);
+    // console.log(winner);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    }
+    
+  return (
+    <>
+    <div className="status">{status}</div>
+    <div className="board">
+    <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
+    <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+    <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
+    <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+    <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+    <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
+    <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+    <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+    <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
+    </div>
+    </>
+  )
+  
+}
+
+
+
+function calculateWinner(squares) {
+  const rules = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+  ];
+  for (let i = 0; i < rules.length; i++) {
+    const [a, b, c] = rules[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
